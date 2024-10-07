70b8539d2482b18aadb85ba1a209bdfe2053f2cf
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-10-07 15:40:44 +0200

    Add project files.

---
+++ UPPGIFT 2,1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string bok = Console.ReadLine();
+            Console.WriteLine(" Hej {Apan i lådan}, den är en väldigt bra bok ");
+        }
+    }
+}
+                  
